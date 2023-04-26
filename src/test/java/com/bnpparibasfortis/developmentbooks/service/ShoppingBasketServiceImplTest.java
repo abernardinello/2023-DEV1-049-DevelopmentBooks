5362245fe261e4a65486a5ab874d0caa60554dd5
Alexis Bernardinello <bernardinello.alexis@gmail.com>
2023-04-26 16:40:13 +0200

    test compute basket with 3 elements

--- src/test/java/com/bnpparibasfortis/developmentbooks/service/ShoppingBasketServiceImplTest.java
+++ src/test/java/com/bnpparibasfortis/developmentbooks/service/ShoppingBasketServiceImplTest.java
@@ -41,12 +41,24 @@ public class ShoppingBasketServiceImplTest {
     }
 
     @Test
-    public void givenShoppingBasketWith2DiferentBooks_whenComputePrice_thenReturnPriceWith5PercentDiscount() {
+    public void givenShoppingBasketWith2DifferentBooks_whenComputePrice_thenReturnPriceWith5PercentDiscount() {
         Book book = new Book("Clean Code", "Robert Martin", "2008");
         Book book2 = new Book("The Clean Coder", "Robert Martin", "2011");
         SHOPPING_CART.add(book);
         SHOPPING_CART.add(book2);
         Float price = service.computePrice(SHOPPING_CART);
-        Assertions.assertEquals((book.getPrice() + book.getPrice()) * 0.95f, price);
+        Assertions.assertEquals((book.getPrice() + book2.getPrice()) * 0.95f, price);
+    }
+    
+    @Test
+    public void givenShoppingBasketWith3DifferentBooks_whenComputePrice_thenReturnPriceWith10PercentDiscount() {
+        Book book = new Book("Clean Code", "Robert Martin", "2008");
+        Book book2 = new Book("The Clean Coder", "Robert Martin", "2011");
+        Book book3 = new Book("Clean Architecture", "Robert Martin", "2017");
+        SHOPPING_CART.add(book);
+        SHOPPING_CART.add(book2);
+        SHOPPING_CART.add(book3);
+        Float price = service.computePrice(SHOPPING_CART);
+        Assertions.assertEquals((book.getPrice() + book2.getPrice() + book3.getPrice()) * 0.90f, price);
     }
 }
