1255dbaeaa5fdfb0c7b88f1a403d08adc9312e71
Alexis Bernardinello <bernardinello.alexis@gmail.com>
2023-04-26 14:49:59 +0200

    Compute empty basket

--- src/main/java/com/bnpparibasfortis/developmentbooks/service/ShoppingBasketServiceImpl.java
+++ src/main/java/com/bnpparibasfortis/developmentbooks/service/ShoppingBasketServiceImpl.java
@@ -1,8 +1,15 @@
 package com.bnpparibasfortis.developmentbooks.service;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
+
+import com.bnpparibasfortis.developmentbooks.model.Book;
 
 @Service
 public class ShoppingBasketServiceImpl implements ShoppingBasketService {
-
+    @Override
+    public Float computePrice(List<Book> shoppingBasket) {
+        return shoppingBasket.isEmpty() ? 0f : null;
+    }
 }
